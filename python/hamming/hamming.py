221e1311751cb7844c788ca37e8f4249a5a020ae
Tom Booster <tom.booster@gmail.com>
2016-04-04 22:59:25 -0700

    Submit hamming.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,4 +2,9 @@
 
 
 def distance(s1, s2):
-    return 0
+    '''
+    Return the Hamming distance between two sequences of characters.
+    '''
+    assert(len(s1) == len(s2))
+
+    return sum(s1[i] != s2[i] for i in range(len(s1)))
--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,26 +1,41 @@
 # -*- coding: utf-8 -*-
-'''
-The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G)
-and thymine (T).
-
-The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G)
-and uracil (U).
-
-Given a DNA strand, its transcribed RNA strand is formed by replacing each
-nucleotide with its complement:
-
-  G -> C
-  C -> G
-  T -> A
-  A -> U
-'''
+"""Solution to exercism:python/rna-transcription."""
 
 
-def to_rna(dna):
-    dna_to_rna = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+def to_rna(dna_sequence):
+    """
+    Transcribe a sequence of DNA nucleotides to RNA nucleotides.
 
-    return "".join([dna_to_rna[c] for c in dna.upper()])
+    Parameters
+    ----------
+    dna_sequence : str
+        A sequence of characters in ('A', 'C', 'G', 'T') representing
+        the DNA sequence.
 
+    Returns
+    -------
+    rna_sequence : str
+        The RNA sequence transcribed from dna_sequence.
 
-if __name__ == '__main__':
-    print(to_rna('ACGTGGTCTTAA'))  # 'UGCACCAGAAUU'
+    Notes
+    -----
+    DNA-RNA transcription algorithm:
+        `G` -> `C`,  `C` -> `G`, `T` -> `A`, `A` -> `U`
+    All characters in dna_sequence must be in [GgCcTtAa], else KeyError.
+
+    Examples
+    --------
+    >>> to_rna('GCTA')
+    'CGAU'
+    >>> to_rna('atcg')
+    'UAGC'
+    """
+    # Create a dictionary to look up RNA characters
+    dna_to_rna = {
+        'A': 'U',
+        'C': 'G',
+        'G': 'C',
+        'T': 'A',
+        }
+
+    return ''.join(dna_to_rna[c] for c in dna_sequence.upper())
