51575ffdf3d38aeb9a54d1ebb89dda0d25c9435a
Tom Booster <tom.booster@gmail.com>
2016-04-06 11:16:51 -0700

    Submit word-count exercise.

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -2,12 +2,14 @@
 import re
 
 
-def word_count(line):
-    print(line)
-    words = re.split(r'[\s\t\n]+', line)
-    for word in words:
-        print(word)
-    return 0
+def word_count(s):
+    d = dict()
+    l = re.split(r'[\W_]+', s.lower())
+    for word in l:
+        if word:
+            # could use defaultdict instead
+            d[word] = d.get(word, 0) + 1
+    return d
 
 if __name__ == '__main__':
-    print(word_count(' one two three'))
+    print(word_count(' no go Go go_go '))
