f2758b7e04cf020c3a5ab51e9bad65fdb275a8a1
Tom Booster <tom.booster@gmail.com>
2016-04-06 20:12:17 -0700

    bob: Submit.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,14 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-
-
 def hey(what):
+    ws = what.strip()
+    if not ws:
+        return 'Fine. Be that way!'
+    elif ws.isupper():
+        return 'Whoa, chill out!'
+    elif ws[-1] == '?':
+        return 'Sure.'
+    else:
+        return 'Whatever.'
 
-    return
+
+if __name__ == '__main__':
+    print(hey('4?'))
